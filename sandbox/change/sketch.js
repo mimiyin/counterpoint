720115dea105f279d630479c84d363ec4f55b32f
Mimi Yin <mimiyin@gmail.com>
2026-02-05 17:13:00 -0500

    More accurate change detection.

--- sandbox/change/sketch.js
+++ sandbox/change/sketch.js
@@ -2,18 +2,20 @@ let cx, cw, sz;
 let a = 0;
 let aspeed = 0.1;
 let adir = 1;
-let d = 1;
 let ds = [];
 let cs = [];
-let avg_d = 1;
-let avg_c = 1;
-let cd = 0;
+let then = 0;
 let y = 0;
 
 let follow = false;
+let loop = true;
 
-const TH = 0.34;
+const TH = 1;
 const M = TH * 100;
+
+const FR = 30;
+const D_DUR = FR * 0.1; // 1/10th of a second
+const C_DUR = FR * 0.2; // 1/2 of a second
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -21,48 +23,65 @@ function setup() {
   cy = height / 2;
   sz = width / 5;
   rectMode(CENTER);
-  frameRate(30);
+  frameRate(FR);
   background(0);
+}
+
+function keyPressed() {
+  if (keyCode == '32') loop = !loop;
+  if (loop) loop();
+  else noLoop();
 }
 
 function draw() {
   background(0, 2);
   fill(255);
   noStroke();
-  //if (frameCount % 6 == 0) {
-  let _cd = calc();
-  if(_cd) rect(0, y, _cd * 10, 1);
-  y++;
-  y %= height;
 
-  // Calculate normalized acc
-  let ncd = abs(cd - _cd) / cd;
-  console.log(ncd);
+  // Recent change normalized by change over time
+  let avgs = calc();
+  console.log(avgs);
+  let avg_d = avgs.avg_d;
+  let avg_c = avgs.avg_c;
 
-  if (ncd) {
-    let xccd = ncd * 100;
-    rect(width - xccd, y, xccd, 1);
-    stroke("red");
-    line(width - M, 0, width - M, height);
-    noStroke();
+  let _cd = avg_d > avg_c ? avg_d / avg_c : avg_c / avg_d;
+  let cd = floor(_cd);
+  if (_cd) {
+    fill(cd > TH ? 'red' : 'white');
+    rect(0, y, _cd * 10, 1);
   }
+
+  // Display speed
+  let d = ds.slice(-1);
+  rect(width - d, y, 10, 1);
+  stroke("red");
+  line(width - M, 0, width - M, height);
+  noStroke();
 
   // Erase change feedback
   fill(0);
   rect(cx, cy, sz);
-  if (ncd >= TH && ncd !== Infinity && ncd !== isNaN) {
-    //fill("red");
-    //rect(cx, cy, sz);
-    //aspeed *= 10;
-    // Should I follow or not?
+
+  // Should we follow?
+  let now = millis(); // Debounce 
+  if (cd > TH && now - then > 1000) {
     follow = random(1) > 0.8;
-    //console.log('follow?', follow);
     fill('red');
     noStroke();
     rect(mouseX, mouseY, 5, 5);
+    textSize(24);
+    text(cd, mouseX, mouseY);
+    // Store timestamp
+    then = now;
   }
-  
-  if(follow) {
+
+
+  // Move down
+  y++;
+  y %= height;
+
+
+  if (follow) {
     aspeed = avg_c;
     //console.log(nfs(aspeed, 0, 2));    
   }
@@ -73,9 +92,6 @@ function draw() {
   fill(255, a);
   noStroke();
   rect(cx, cy, sz);
-  
-  // Update for next frame
-  cd = _cd;
 
   // Text labels
   noStroke();
@@ -85,32 +101,35 @@ function draw() {
   textAlign(LEFT, TOP);
   text("recent/older avg", 10, 0);
   textAlign(RIGHT, TOP);
-  text("acc", width-10, 0);
+  text("acc", width - 10, 0);
 
 }
 
 
 // Calculate speed: recent average (10f) over older average (50f)
 function calc() {
+
+  // Calculate distance traveled (speed)
   let d = dist(mouseX, mouseY, pmouseX, pmouseY);
 
   // Update
+  d = floor(d);
   ds.push(d);
-  if (ds.length > 10) {
+  if (ds.length > D_DUR) {
     cs.push(ds.shift());
   }
 
   // New now average
   let d_sum = 0;
   for (let d of ds) d_sum += d;
-  avg_d = d_sum / ds.length;
+  let avg_d = d_sum / ds.length;
 
   // New trend average
   let c_sum = 0;
   for (let c of cs) c_sum += c;
-  avg_c = c_sum / cs.length;
-  if (cs.length > 50) cs.shift();
+  let avg_c = c_sum / cs.length;
+  if (cs.length > C_DUR) cs.shift();
 
-  // console.log("!", nfs(avg_d/avg_c, 0, 2));
-  return avg_d / avg_c;
+  console.log("!", D_DUR);
+  return { avg_d: avg_d, avg_c: avg_c }
 }
